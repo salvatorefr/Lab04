ee426e111f6296463ccd95eea0046188ff360041
tattommasi <47092404+tattommasi@users.noreply.github.com>
2019-04-13 01:22:22 +0200

    soluzione

--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/model/Studente.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/model/Studente.java
@@ -1,5 +1,101 @@
 package it.polito.tdp.lab04.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Studente {
-	
+
+	private int matricola;
+	private String cognome;
+	private String nome;
+	private String cds;
+
+	public Studente() {
+	}
+
+	public Studente(int matricola) {
+		this.matricola = matricola;
+	}
+
+	public Studente(int matricola, String cognome, String nome, String cds) {
+		this.matricola = matricola;
+		this.cognome = cognome;
+		this.nome = nome;
+		this.cds = cds;
+	}
+
+	/*
+	 * Getters and Setters
+	 */
+	public int getMatricola() {
+		return matricola;
+	}
+
+	public void setMatricola(int matricola) {
+		this.matricola = matricola;
+	}
+
+	public String getCognome() {
+		if (cognome == null)
+			return "";
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getNome() {
+		if (nome == null)
+			return "";
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCds() {
+		if (cds == null)
+			return "";
+		return cds;
+	}
+
+	public void setCds(String cds) {
+		this.cds = cds;
+	}
+
+	/*
+	 * Hash Code and Equals
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + matricola;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Studente other = (Studente) obj;
+		if (matricola != other.matricola)
+			return false;
+		return true;
+	}
+
+	/*
+	 * Metodo toString()
+	 */
+	@Override
+	public String toString() {
+		return cognome + " " + nome;
+	}
+
 }
